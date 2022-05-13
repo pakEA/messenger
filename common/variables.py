2ded6c283b9d793d7e954e633bbf10b17348cbfa
ea <pak_ea@inbox.ru>
2022-05-13 03:18:50 -0700

    Initial commit

---
+++ common/variables.py
@@ -0,0 +1,25 @@
+# Порт по умолчанию
+DEFAULT_PORT = 7777
+
+# IP-адрес по умолчанию
+DEFAULT_IP_ADRESS = '127.0.0.1'
+
+# Максимальная очередь подключений
+MAX_CONNECTIONS = 5
+
+# Максимальная длина сообщения в байтах
+MAX_PACKAGE_LENGTH = 1024
+
+# Кодировка
+ENCODING = 'utf-8'
+
+# Основные ключи протокола JIM (JSON instant messaging):
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+# Прочие ключи
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
